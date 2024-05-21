7e9ad8396194e6e8f80ebb079c38662a96567d70
mygoaway <mygoaway2@gmail.com>
2024-05-21 12:08:08 +0900

    섹션 3. 컬렉션 프레임워크 - ArrayList 강의 후 재커밋

--- src/collection/array/MyArrayListV4Main.java
+++ src/collection/array/MyArrayListV4Main.java
@@ -18,5 +18,9 @@ public class MyArrayListV4Main {
         intList.add(3);
         Integer integer = intList.get(0);
         System.out.println("integer = " + integer);
+
+        // 배열, 리스트는 순서대로 마지막에 데이터를 추가하거나 삭제할 때는 성능이 좋지만,
+        // 앞이나 중간에 데이터를 추가하거나 삭제할 때는 성능이 좋지 않다.
+        // 이런 단점을 해결한 자료 구조인 링크드 리스트(LinkedList)에 대해서 알아보자.
     }
 }
