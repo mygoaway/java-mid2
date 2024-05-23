31539fff9546f2e80a3be31b100740ca97859a0e
mygoaway <mygoaway2@gmail.com>
2024-05-23 12:40:46 +0900

    섹션 6. 해시(Hash) 강의 후 재커밋

--- src/collection/set/HashStart4.java
+++ src/collection/set/HashStart4.java
@@ -3,20 +3,39 @@ package collection.set;
 import java.util.Arrays;
 
 public class HashStart4 {
+
+    static final int CAPACITY = 10;
+
     public static void main(String[] args) {
-        Integer[] inputArray = new Integer[100];
-        inputArray[1] = 1;
-        inputArray[2] = 2;
-        inputArray[5] = 5;
-        inputArray[8] = 8;
-        inputArray[14] = 14;
-        inputArray[99] = 99;
+        // {1, 2, 5, 8, 14, 99}
+        System.out.println("hashIndex(1) = " + hashIndex(1));
+        System.out.println("hashIndex(2) = " + hashIndex(2));
+        System.out.println("hashIndex(5) = " + hashIndex(5));
+        System.out.println("hashIndex(8) = " + hashIndex(8));
+        System.out.println("hashIndex(14) = " + hashIndex(14));
+        System.out.println("hashIndex(99) = " + hashIndex(99));
+
+        Integer[] inputArray = new Integer[CAPACITY];
+        add(inputArray, 1);
+        add(inputArray, 2);
+        add(inputArray, 5);
+        add(inputArray, 8);
+        add(inputArray, 14);
+        add(inputArray, 99);
         System.out.println(Arrays.toString(inputArray));
-        
-        int searchValue = 99;
-        Integer result = inputArray[searchValue]; // 0(1)
+
+        int searchValue = 14;
+        Integer result = inputArray[hashIndex(searchValue)]; // 0(1)
         System.out.println("result = " + result);
-        // 데이터의 값을 인덱스로 사용한 덕분에 0(1)의 매우 빠른 검색 속도를 얻을 수 있었다.
-        // 하지만, 낭비되는 메모리 공간이 너무 많다.
     }
+
+    private static void add(Integer[] inputArray, int value) {
+        int hashIndex = hashIndex(value);
+        inputArray[hashIndex] = value;
+    }
+
+    private static int hashIndex(int value) {
+        return value % CAPACITY;
+    }
+
 }
--- src/collection/set/HashStart5.java
+++ src/collection/set/HashStart5.java
@@ -1,6 +1,7 @@
 package collection.set;
 
 import java.util.Arrays;
+import java.util.LinkedList;
 
 public class HashStart5 {
 
@@ -8,34 +9,44 @@ public class HashStart5 {
 
     public static void main(String[] args) {
         // {1, 2, 5, 8, 14, 99}
-        System.out.println("hashIndex(1) = " + hashIndex(1));
-        System.out.println("hashIndex(2) = " + hashIndex(2));
-        System.out.println("hashIndex(5) = " + hashIndex(5));
-        System.out.println("hashIndex(8) = " + hashIndex(8));
-        System.out.println("hashIndex(14) = " + hashIndex(14));
-        System.out.println("hashIndex(99) = " + hashIndex(99));
+        LinkedList<Integer> [] buckets = new LinkedList[CAPACITY];
+        System.out.println(Arrays.toString(buckets));
 
-        Integer[] inputArray = new Integer[CAPACITY];
-        add(inputArray, 1);
-        add(inputArray, 2);
-        add(inputArray, 5);
-        add(inputArray, 8);
-        add(inputArray, 14);
-        add(inputArray, 99);
-        System.out.println(Arrays.toString(inputArray));
+        for(int i = 0; i < CAPACITY; i++) {
+            buckets[i] = new LinkedList<>();
+        }
+        System.out.println(Arrays.toString(buckets));
 
-        int searchValue = 14;
-        Integer result = inputArray[hashIndex(searchValue)]; // 0(1)
-        System.out.println("result = " + result);
+        add(buckets, 1);
+        add(buckets, 2);
+        add(buckets, 5);
+        add(buckets, 8);
+        add(buckets, 14);
+        add(buckets, 99);
+        add(buckets, 9); // 중복 => 해시 충돌 발생
+        System.out.println(Arrays.toString(buckets));
+
+        // 검색
+        int searchValue = 9;
+        boolean result = contains(buckets, searchValue);
+        System.out.println("buckets.contains(" + searchValue + ") = " + result);
     }
 
-    private static void add(Integer[] inputArray, int value) {
+    private static void add(LinkedList<Integer> [] buckets, int value) {
         int hashIndex = hashIndex(value);
-        inputArray[hashIndex] = value;
+        LinkedList<Integer> bucket = buckets[hashIndex]; //0(1)
+        if(!bucket.contains(value)) { // O(n)
+            bucket.add(value);
+        }
     }
 
     private static int hashIndex(int value) {
         return value % CAPACITY;
     }
 
+    private static boolean contains(LinkedList<Integer> [] buckets, int value) {
+        int hashIndex = hashIndex(value);
+        LinkedList<Integer> bucket = buckets[hashIndex]; // 0(1)
+        return bucket.contains(value); // 0(n)
+    }
 }
