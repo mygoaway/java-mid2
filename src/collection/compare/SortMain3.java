80c2f7ff17edb88ff7f323c7fc69218f67ad3de7
mygoaway <mygoaway2@gmail.com>
2024-05-30 23:48:56 +0900

    섹션 10. 컬렉션 프레임워크 - 순회, 정렬, 전체 정리 강의 커밋

--- src/collection/compare/SortMain3.java
+++ src/collection/compare/SortMain3.java
@@ -5,7 +5,6 @@ import java.util.Comparator;
 
 public class SortMain3 {
     public static void main(String[] args) {
-        MyUser[] myUsers = new MyUser[4];
         MyUser myUser1 = new MyUser("a", 30);
         MyUser myUser2 = new MyUser("b", 20);
         MyUser myUser3 = new MyUser("c", 10);
