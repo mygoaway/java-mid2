e937f924cfc0b196afbba831eaf50275c85e2ede
mygoaway <mygoaway2@gmail.com>
2024-06-04 23:46:03 +0900

    섹션 10. 컬렉션 프레임워크 - 순회, 정렬, 전체 정리 강의 커밋

--- src/collection/utils/OfMain.java
+++ src/collection/utils/OfMain.java
@@ -6,11 +6,12 @@ import java.util.Set;
 
 public class OfMain {
     public static void main(String[] args) {
+        
         // 편리한 불변 컬렉션
         List<Integer> list = List.of(1, 2, 3);
         Set<Integer> set = Set.of(1, 2, 3);
         Map<Integer, String> map = Map.of(1, "One", 2, "Two");
-        
+
         // 값을 추가하거나 삭제하는 경우 컴파일 에러 발생
         //list.remove(1);
         System.out.println("list = " + list);
